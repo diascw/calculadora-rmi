e38feff16c7f76a7c3d0994a7d8b4ead39b6924b
diascw <wanessadiascw@gmail.com>
2025-05-17 16:18:54 -0300

    feat: implementa regras de negocio

--- src/CalculadoraImpl.java
+++ src/CalculadoraImpl.java
@@ -1,3 +1,31 @@
-public class CalculadoraImpl {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
+public class CalculadoraImpl extends UnicastRemoteObject implements Calculadora {
+
+    protected CalculadoraImpl() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public double somar(double a, double b) throws RemoteException {
+        return a + b;
+    }
+
+    @Override
+    public double subtrair(double a, double b) throws RemoteException {
+        return a - b;
+    }
+
+    @Override
+    public double multiplicar(double a, double b) throws RemoteException {
+        return a * b;
+    }
+
+    @Override
+    public double dividir(double a, double b) throws RemoteException {
+        if (b == 0)
+            throw new ArithmeticException("divisão por zero não eh permitida.");
+        return a / b;
+    }
 }

05eca0b9eabe0dbb7a0e9761a7743b7fe51bb703
diascw <wanessadiascw@gmail.com>
2025-05-17 16:21:30 -0300

    feat: implementa cliente RMI

--- src/CalculadoraClient.java
+++ src/CalculadoraClient.java
@@ -1,3 +1,17 @@
-public class CalculadoraClient {
+import java.rmi.Naming;
 
+public class CalculadoraClient {
+    public static void main(String[] args) {
+        try {
+            Calculadora calculadora = (Calculadora) Naming.lookup("rmi://localhost:1099/CalculadoraService");
+
+            System.out.println("5 + 3 = " + calculadora.somar(5, 3));
+            System.out.println("10 - 2 = " + calculadora.subtrair(10, 2));
+            System.out.println("4 * 6 = " + calculadora.multiplicar(4, 6));
+            System.out.println("8 / 2 = " + calculadora.dividir(8, 2));
+        } catch (Exception e) {
+            System.err.println("erro no cliente: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }

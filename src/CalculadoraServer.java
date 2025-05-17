e0755008f83d8db994b7df3c92554cc5bbe183d8
diascw <wanessadiascw@gmail.com>
2025-05-17 16:20:03 -0300

    feat: adiciona servidor RMI com registro da calculadora

--- src/CalculadoraServer.java
+++ src/CalculadoraServer.java
@@ -1,3 +1,19 @@
-public class CalculadoraServer {
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
+public class CalculadoraServer {
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099);
+
+            Calculadora calculadora = new CalculadoraImpl();
+
+            Naming.rebind("rmi://localhost:1099/CalculadoraService", calculadora);
+
+            System.out.println("servidor RMI pronto e aguardando conexões...");
+        } catch (Exception e) {
+            System.err.println("erro no servidor: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
